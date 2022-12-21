4a884e2a8e8445e7c9a7b1d167b3e2b34c2852ef
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-12-21 13:32:36 +0100

    rm node modules

--- verifier-integration/js/node_modules/base58-js/public/index.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-exports.base58_to_binary = require('./base58_to_binary.js')
-exports.binary_to_base58 = require('./binary_to_base58.js')
--- verifier-integration/js/node_modules/base58-js/public/index.mjs
+++
@@ -1,2 +0,0 @@
-export { default as binary_to_base58 } from './binary_to_base58.js'
-export { default as base58_to_binary } from './base58_to_binary.js'
