5183cdb4585fcc6de7fbf9fe722aba89e2e57eea
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2023-03-09 15:23:54 +0200

    remove test keys

--- on-chain-verification/hardhat.config.js
+++ on-chain-verification/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   networks: {
     mumbai: {
       chainId: 80001,
-      url: "https://polygon-mumbai.g.alchemy.com/v2/BI7_GYAO787OflUC7E6DhMJNkhkyq7kp",
-      accounts: ["dc118b5238d3be5b562791b5adc2b148003efe0e1285073f1540fec5b14ba337"],
+      url: `${process.env.ALCHEMY_MUMBAI_URL}`,
+      accounts: [`0x${process.env.MUMBAI_PRIVATE_KEY}`]
     }
   }
 };
